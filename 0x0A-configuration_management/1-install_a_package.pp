d46b273e40f7a98fa19dfa2881b91401c0b96ce8
Mimifresh <mimigolld@gmail.com>
2023-11-24 21:08:32 +0100

    fixed merge issues

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,10 @@
-#install flask
+# Install Werkzeug
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
+# Install Flask
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
