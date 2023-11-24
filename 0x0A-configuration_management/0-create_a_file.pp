247f67eaf0f3bed49db8fcd236bb7365f853f4f3
Mimifresh <mimigolld@gmail.com>
2023-11-24 13:55:54 +0100

    created a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
-#creates a file
-file { /tmp/school:
-ensure => present,
-mode => 0744,
-owner => www-data,
-group => www-data,
-content => 'I love Puppet',
-} 
+file { '/tmp/school':
+  ensure  => present,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
