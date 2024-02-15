78c6a07eba2eb947bae32d20bf3973029ed89d01
Mimifresh <mimigolld@gmail.com>
2024-02-15 08:02:43 +0100

    employee todo list

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" A python script that return information 
+""" A python script that return information
 about an employee's TODO list progress"""
 
 import requests
@@ -8,7 +8,8 @@ import sys
 if __name__ == "__main__":
     link = "https://jsonplaceholder.typicode.com/"
     person = requests.get(link + "users/{}".format(sys.argv[1])).json()
-    tasks = requests.get(link + "todos", params={"usersId": sys.argv[1]}).json()
+    tasks = requests.get(link + "todos",
+                         params={"usersId": sys.argv[1]}).json()
 
     done = [i.get("title") for i in tasks if i.get("completed") is True]
     print("Employee {} is done with tasks({}/{}): ".format(
